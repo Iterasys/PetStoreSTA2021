1ef64d2d9ee1d00f21db5323b873edaf5e93d05d
Iterasys <40407006+Iterasys@users.noreply.github.com>
2021-08-11 22:28:51 -0300

    Criei o teste para incluir um pet

---
+++ src/test/java/petstore/Pet.java
@@ -0,0 +1,49 @@
+// 1 - Pacote
+package petstore;
+
+// 2 - Bibliotecas
+
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+
+
+// 3 - Classe
+public class Pet {
+    // 3.1 - Atributos
+    String uri = "https://petstore.swagger.io/v2/pet"; // endereço da entidade Pet
+
+    // 3.2 - Métodos e Funções
+    public String lerJson(String caminhoJson) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(caminhoJson)));
+    }
+
+    // Incluir - Create - Post
+    @Test  // Identifica o método ou função como um teste para o TestNG
+    public void incluirPet() throws IOException {
+        String jsonBody = lerJson("db/pet1.json");
+
+        // Sintaxe Gherkin
+        // Dado - Quando - Então
+        // Given - When - Then
+
+        given() // Dado
+                .contentType("application/json") // comum em API REST - antigas era "text/xml"
+                .log().all()
+                .body(jsonBody)
+        .when()  // Quando
+                .post(uri)
+        .then()  // Então
+                .log().all()
+                .statusCode(200)
+        ;
+
+
+    }
+
+
+}
