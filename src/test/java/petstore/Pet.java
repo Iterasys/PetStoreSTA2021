96977a5391e63a9454f5fed207d2a29e80045c68
Iterasys <40407006+Iterasys@users.noreply.github.com>
2021-08-12 20:38:56 -0300

    Completei o script incluirPet (Post)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -11,6 +11,7 @@ import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
 
 // 3 - Classe
 public class Pet {
@@ -42,6 +43,8 @@ public class Pet {
                 .statusCode(200)
                 .body("name", is("Atena"))
                 .body("status", is("available"))
+                .body("category.name", is("dog"))
+                .body("tags.name", contains("sta"))
         ;
 
 
