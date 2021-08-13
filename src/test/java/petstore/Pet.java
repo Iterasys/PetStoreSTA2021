407f215dbccff4b8d2c766787d0eeed1d1de560e
Iterasys <40407006+Iterasys@users.noreply.github.com>
2021-08-12 22:05:15 -0300

    Finalizado o script excluirPet (Delete)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -71,7 +71,44 @@ public class Pet {
         ;
         System.out.println("O token é " + token);
 
-
-
     }
+
+    @Test(priority=3)
+    public void alterarPet() throws IOException {
+        String jsonBody = lerJson("db/pet2.json");
+
+        given()
+                .contentType("application/json")
+                .log().all()
+                .body(jsonBody)
+        .when()
+                .put(uri)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Atena"))
+                .body("status",is("sold"))
+        ;
+    }
+
+    @Test (priority = 4)
+    public void excluirPet(){
+        String petId = "1974080145";
+
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .delete(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is("unknown"))
+                .body("message", is(petId))
+
+        ;
+    }
+
+
 }
